88d8ad2b3e4b5697e6c94ff4be8329edecd9013b
jxnkwlp <jxnkwlp@hotmail.com>
2021-09-24 18:33:52 +0800

    #6 Add 'QueryTranslator' tests

--- test/EasySql.Tests/SqlTranslatorTests.cs
+++ test/EasySql.Tests/SqlTranslatorTests.cs
@@ -1,35 +1,35 @@
-﻿using System.Linq;
-using EasySql.SqlServer;
-using EasySql.Tests.Entities;
-using NUnit.Framework;
+﻿//using System.Linq;
+//using EasySql.SqlServer;
+//using EasySql.Tests.Entities;
+//using NUnit.Framework;
 
-namespace EasySql.Tests
-{
-    public class SqlTranslatorTests
-    {
-        private DbContext _dbContext = null;
+//namespace EasySql.Tests
+//{
+//    public class SqlTranslatorTests
+//    {
+//        private DbContext _dbContext = null;
 
-        [SetUp]
-        public void Setup()
-        {
-            _dbContext = new DbContext(b =>
-            {
-                b.UseSqlServer("server=.;database=Northwind;uid=sa;password=Pass@123456");
-            });
+//        [SetUp]
+//        public void Setup()
+//        {
+//            _dbContext = new DbContext(b =>
+//            {
+//                b.UseSqlServer("server=.;database=Northwind;uid=sa;password=Pass@123456");
+//            });
 
-        }
+//        }
 
-        [Test]
-        public void Where_Conditions_1()
-        {
-            var query = _dbContext.Query<Order>();
+//        [Test]
+//        public void Where_Conditions_1()
+//        {
+//            var query = _dbContext.Query<Order>();
 
-            var sql = query.Where(x => x.Id > 10).ToSqlText();
+//            var sql = query.Where(x => x.Id > 10).ToSqlText();
 
-            Assert.IsTrue(sql == @"SELECT ""o"".""OrderID"" , ""o"".""CustomerID"" , ""o"".""EmployeeID"" , ""o"".""OrderDate"" , ""o"".""RequiredDate"" , ""o"".""ShipName""
-FROM ""Orders"" AS ""o""
-WHERE ""o"".""OrderID"" > 10");
+//            Assert.IsTrue(sql == @"SELECT ""o"".""OrderID"" , ""o"".""CustomerID"" , ""o"".""EmployeeID"" , ""o"".""OrderDate"" , ""o"".""RequiredDate"" , ""o"".""ShipName""
+//FROM ""Orders"" AS ""o""
+//WHERE ""o"".""OrderID"" > 10");
 
-        }
-    }
-}
+//        }
+//    }
+//}
